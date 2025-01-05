c30efd5db35a1bc228116fb6b3f707fa786c7852
JungHyeonKim <dev.kjung@gmail.com>
2025-01-05 11:31:37 +0900

    [Feat]: 프로젝트 생성

---
+++ src/main/java/com/kjung/springrestboilerplate/SpringRestBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.kjung.springrestboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjung/springrestboilerplate/SpringRestBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjung.springrestboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
